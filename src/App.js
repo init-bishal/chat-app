4d531def0548f72ac3d8c998c749a6334d732d9f
Bishal Ghosh <bbishal.g01@gmail.com>
2023-08-09 23:47:43 +0530

    public and private routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import 'rsuite/dist/rsuite.min.css';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 import './styles/main.scss'
+import { Switch,Route } from 'react-router';
+import PrivateRoute from './components/PrivateRoute'
+import PublicRoute from './components/PublicRoute';
 function App() {
   return (
-    <div>
-        Hello
-    </div>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn/>
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home/>
+        </PrivateRoute>
+      </Switch>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Homekrn3fj 
+        </div>
+    )
+}
+
+export default Home
