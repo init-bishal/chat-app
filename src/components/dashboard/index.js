a7a76d6bc1e2f3b1c0b0c93929eb44fe1d10d658
Bishal Ghosh <bbishal.g01@gmail.com>
2023-08-15 11:09:02 +0530

    Provider Block

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Drawer,Button, Divider } from 'rsuite'
+import { Drawer,Button, Divider, Alert } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
 import EditableInput from '../EditableInput'
+import { database } from '../../misc/firebase'
+import ProviderBlock from './ProviderBlock'
 const Dashboard = ({onSignOut}) => {
-    const onSave=async (newData)=>{
-        console.log(newData)
-    }
+    
     const {profile}=useProfile()
+    const onSave=async (newData)=>{
+            const userNicknameRef=database.ref(`/profile/${profile.uid}`).child('name')
+            //name is the field which we want to update
+            try{
+                await userNicknameRef.set(newData)
+                Alert.success("Nickname has been updated")
+            }
+            catch(err)
+            {
+                Alert.error(err.message,4000)
+            }
+        }
     return (
         <>
             <Drawer.Header>
@@ -16,6 +28,7 @@ const Dashboard = ({onSignOut}) => {
             </Drawer.Header>
             <Drawer.Body>
                 <h3>Hey {profile.name}</h3>
+                <ProviderBlock/>
                 <Divider/>
                 <EditableInput
                     name="nickname"
